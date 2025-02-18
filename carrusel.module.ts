3706747ae744f84dd6012567fce0835811f5ceff
katisa-sa <99440611+katisa-sa@users.noreply.github.com>
2025-02-18 23:55:54 +0100

    Add files via upload

---
+++ carrusel.module.ts
@@ -0,0 +1,25 @@
+
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+import { CarruselPageRoutingModule } from './carrusel-routing.module';
+import { CarruselPage } from './carrusel.page';
+import { MisComponentesModule } from '../../components/mis-componentes.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CarruselPageRoutingModule,
+    MisComponentesModule,
+  ],
+  declarations: [
+    CarruselPage
+  ]
+
+})
+export class CarruselPageModule {}
