654ac8c695c0955517527207d61f83d2b1d1ba68
katisa-sa <99440611+katisa-sa@users.noreply.github.com>
2025-02-18 23:58:28 +0100

    Update carrusel.module.ts

--- src/app/pages/carrusel/carrusel.module.ts
+++ src/app/pages/carrusel/carrusel.module.ts
@@ -1,27 +1,24 @@
-
-import { MisComponentesModule } from './../../components/mis-componentes.module';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 import { CarruselPageRoutingModule } from './carrusel-routing.module';
 import { CarruselPage } from './carrusel.page';
-
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+import { MisComponentesModule } from '../../components/mis-componentes.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     CarruselPageRoutingModule,
-    MisComponentesModule
+    MisComponentesModule,
   ],
-  declarations: [CarruselPage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [
+    CarruselPage
+  ]
+
 })
 export class CarruselPageModule {}
